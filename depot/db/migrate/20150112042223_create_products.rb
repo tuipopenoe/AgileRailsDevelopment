3524834d87e3cebb89f42c706efdf027e23c8e71
Tui Popenoe <tuipopenoe@gmail.com>
2015-01-11 23:20:22 -0600

    Rails 4.0 updates

--- depot/db/migrate/20150112042223_create_products.rb
+++ depot/db/migrate/20150112042223_create_products.rb
@@ -4,9 +4,9 @@ class CreateProducts < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price, :precision => 8, :scale => 2
+      t.decimal :price, precision: 8, scale: 2
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105192822) do
+ActiveRecord::Schema.define(version: 20150112042223) do
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
